f21ff575d7957f55c8fafbe4d7f510bb3a58bab2
kantawat1156-github <71268120+kantawat1156-github@users.noreply.github.com>
2020-09-22 16:30:03 +0700

    Update Labทบทวน_06.py

--- Labทบทวน_06.py
+++ Labทบทวน_06.py
@@ -1,10 +1,20 @@
-day = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-
 x = int(input())
 y = int(input())
-sun = x
-today = (y-x)%7
-if x >= 1 and x <= 7 and y >= 1 and y <= 31:
-  print(day[today])
-else:
-  print("ERROR")
+z = int(input())
+if x > y and x > z:
+    c = x
+    a = y
+    b = z
+if y > z and y > x:
+    c = y
+    a = x
+    b = z
+if z > x and z > y:
+    c = z
+    a = y
+    b = x
+if x >= 0 and y >= 0 and z >= 0 and x != y and x != z and y != z:
+    if (a**2)+(b**2) - (c**2) == 0:
+        print("True")
+    else:
+        print("False")
